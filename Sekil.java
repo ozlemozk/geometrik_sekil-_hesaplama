e1b3c0bfff59d5837089e97f82a6eb6419ccc6fc
Özlem ÖZKAYA <58657181+ozlemozk@users.noreply.github.com>
2021-02-27 01:13:07 +0300

    Update Sekil.java

--- Sekil.java
+++ Sekil.java
@@ -1,20 +1,14 @@
-
+/*şekiler ve işlemleri*/
 public abstract class Sekil {
     private String isim;
-
-    public Sekil(String isim) {
-        this.isim = isim;
-    }
+ public Sekil(String isim) {
+        this.isim = isim;}
     
     abstract void alanHesapla();
 
     public String getIsim() {
-        return isim;
-    }
+        return isim; }
 
     public void setIsim(String isim) {
         this.isim = isim;
-    }
-    
-    
-}
+    }}
